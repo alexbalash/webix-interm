8daf635e4d316288d0e42dcd6f5fb0e2e2cfadfd
alexbalash <55133359+alexbalash@users.noreply.github.com>
2021-12-31 14:20:13 +0300

    [init] lesson3

---
+++ data.js
@@ -0,0 +1,154 @@
+
+let data = {
+  collection:"Users",
+  records:[
+  {
+    "id": 1,
+    "isActive": true,
+    "balance": 1721.87,
+    "picture": "http://placehold.it/32x32",
+    "age": 28,
+    "eyeColor": "brown",
+    "fname": "Alice ",
+   "lname": "Brown",
+    "gender": "female",
+    "company": "EARTHPURE",
+    "email": "alicebrown@earthpure.com",
+    "phone": "+1 (972) 551-3654",
+    "address": "790 Richardson Street, Tilleda, Arizona, 4325",
+    "registered": "2015-07-27 01:13:41",
+    "friends": [
+      {
+        "id": 0,
+        "name": "Silvia Steele"
+      },
+      {
+        "id": 1,
+        "name": "Charlotte Richard"
+      },
+      {
+        "id": 2,
+        "name": "Sutton Kemp"
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "isActive": true,
+    "balance": 1564.43,
+    "picture": "http://placehold.it/32x32",
+    "age": 38,
+    "eyeColor": "brown",
+    "fname": "Antoinette ",
+    "lname": "Vaughn",
+    "gender": "female",
+    "company": "ZOID",
+    "email": "antoinettevaughn@zoid.com",
+    "phone": "+1 (927) 449-2039",
+    "address": "803 Nautilus Avenue, Stewartville, Rhode Island, 9651",
+    "registered": "2017-12-01 09:43:28",
+    "friends": [
+      {
+        "id": 0,
+        "name": "Sanchez Pope"
+      },
+      {
+        "id": 1,
+        "name": "Kramer Mcleod"
+      },
+      {
+        "id": 2,
+        "name": "Celina Fitzpatrick"
+      }
+    ]
+  },
+  {
+    "id": 3,
+    "isActive": false,
+    "balance": 3835.06,
+    "picture": "http://placehold.it/32x32",
+    "age": 20,
+    "eyeColor": "brown",
+    "fname": "Stevens ",
+    "gender": "male",
+    "company": "MEDALERT",
+    "email": "stevensortega@medalert.com",
+    "phone": "+1 (817) 429-2644",
+    "address": "217 Revere Place, Harborton, Texas, 960",
+    "registered": "2016-03-05 07:33:24",
+    "friends": [
+      {
+        "id": 0,
+        "name": "Inez Orr"
+      },
+      {
+        "id": 1,
+        "name": "Emma Shannon"
+      },
+      {
+        "id": 2,
+        "name": "Andrea Holman"
+      }
+    ]
+  },
+  {
+    "id": 4,
+    "isActive": true,
+    "balance": 2078.11,
+    "picture": "http://placehold.it/32x32",
+    "age": 20,
+    "eyeColor": "brown",
+    "fname": "Garrett ",
+    "lname": "Bray",
+    "gender": "male",
+    "company": "MAINELAND",
+    "email": "garrettbray@maineland.com",
+    "phone": "+1 (894) 524-2102",
+    "address": "405 Virginia Place, Layhill, Iowa, 1088",
+    "registered": "2018-02-11 08:29:28",
+    "friends": [
+      {
+        "id": 0,
+        "name": "Barlow Mason"
+      },
+      {
+        "id": 1,
+        "name": "Bradley Solomon"
+      },
+      {
+        "id": 2,
+        "name": "Colon Ray"
+      }
+    ]
+  },
+  {
+    "id": 5,
+    "isActive": true,
+    "balance": 2397,
+    "picture": "http://placehold.it/32x32",
+    "age": 33,
+    "eyeColor": "green",
+    "fname": "Barrera ",
+    "lname": "Peters",
+    "gender": "male",
+    "company": "DATACATOR",
+    "email": "barrerapeters@datacator.com",
+    "phone": "+1 (807) 441-3714",
+    "address": "323 Chapel Street, Cowiche, California, 4383",
+    "registered": "2018-01-12 12:50:20",
+    "friends": [
+      {
+        "id": 0,
+        "name": "Twila Levine"
+      },
+      {
+        "id": 1,
+        "name": "Dickerson Kim"
+      },
+      {
+        "id": 2,
+        "name": "Melanie Duncan"
+      }
+    ]
+  }
+]};
